92bdd4f3f6a3c07b4fa4c5f52cd6cdd2653b4d2e
sofiapavlova <nab17001@byui.edu>
2021-11-12 08:40:59 +0300

    prophets

--- lesson9/prophets/prophets.js
+++ lesson9/prophets/prophets.js
@@ -6,15 +6,28 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    });
 
-  const prophets = jsonObject['prophets'];
+    const prophets = jsonObject['prophets'];
+    prophets.forEach((prophet) => {
 
-  let card = document.createElement('section');
-let h2 = document.createElement('h2');
+    let card = document.createElement('section');
 
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+    let h2 = document.createElement('h2');
+    h2.innerHTML=`${prophet.name} <span class="highlight">${prophet.lastname}</span>`;
+    card.appendChild(h2);
 
-card.appendChild(h2);
+    let birthplace = document.createElement ('p');
+    birthplace.innerHTML=`Place of birth: ${prophet.birthplace}`;
+    card.appendChild(birthplace);
 
-document.querySelector('div.cards').appendChild(card);
+    let birthdate = document.createElement ('p');
+    birthdate.innerHTML=`Date of birth: ${prophet.birthdate}`;
+    card.appendChild(birthdate);
+
+    let image = document.createElement ('img');
+    image.setAttribute("src", prophet.imageurl);
+    card.appendChild(image);
+
+    document.querySelector('div.cards').appendChild(card);
+  });
+  })
